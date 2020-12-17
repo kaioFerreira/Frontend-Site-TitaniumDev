45293635e12ca65cb322019b39b3e92420970234
kaioFerreira <kaiofhs@hotmail.com>
2020-12-17 14:07:35 -0300

    Fixed: Erro404 In Reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => (
     <>
-        <BrowserRouter>
+        <HashRouter>
             <Routes />
-        </BrowserRouter>
+        </HashRouter>
         <GlobalStyle/>
     </>
 );
